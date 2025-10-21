1cd1861067de293f8348923db7e3b44561250173
Infamous003 <arfathsyed0312@gmail.com>
2025-10-21 13:33:47 +0530

    feat: add user service

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"net/http"
 
+	"github.com/Infamous003/go-blog-backend/service/user"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -24,9 +25,16 @@ func (s *APIServer) Run() error {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
+	apiRouter := chi.NewRouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(apiRouter)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("What's up!"))
 	})
+
+	r.Mount("/api/v1", apiRouter)
 
 	newServer := &http.Server{
 		Addr:    s.addr,
